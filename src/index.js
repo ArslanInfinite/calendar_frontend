23688cd36c83c8197a4a00fe60cb8bec12c5994a
ArslanInfinite <ali.arslan319@gmail.com>
2021-05-23 13:16:43 -0400

    create route for calendarHome using Browser Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route} from 'react-router-dom' //BrowserRouter uses HTML5 API to keep track of UI with the URL. Route tells React to renders certain components when certain routes/url are reached.
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const calendarHome = () => (
+  <div>
+    Islamic Calendar Lite - Homepage
+  </div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //create BrowserRouter instance, this is the parent for all the routes, knows the history for all the routes
+  // div is needed for multiple children (routes) of BrowserRouter
+  // route takes two attributes, the path and component. What component should render when what path is reached. 
+
+const routes = (
+  <BrowserRouter> 
+    <div>
+      <Route path='/' component={calendarHome}/>
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('root')
+); 
